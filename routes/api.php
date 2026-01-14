4c2b583145fa538fa433f7e27c04aaa5e3919662
edenjordan <jordandokou1@gmail.com>
2026-01-14 11:27:48 +0000

    CRUD pour les variantes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\admin\AuthController as AuthAdmin;
 use App\Http\Controllers\users\AuthController as AuthUser;
+use App\Http\Controllers\variantes\VarianteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
@@ -11,6 +12,10 @@ use App\Models\User;
 //     ->middleware(['signed'])
 //     ->name('verification.verify.api');
 
+
+/*
+     Routes ne necessitant pas l'authentification
+ */
 
 Route::prefix('admin')->group(function () {
     Route::post('login', [AuthAdmin::class, 'login']);
@@ -22,4 +27,32 @@ Route::prefix('user')->group(function () {
     // Route::post('verify-email', [AuthUser::class, 'verifyEmail']);
     Route::post('forgot-password', [AuthUser::class, 'forgotPassword']);
     Route::post('reset-password', [AuthUser::class, 'resetPassword']);
+});
+
+// Variantes
+Route::get('/variantes', [VarianteController::class, 'index']);
+Route::get('/variantes/{variante}', [VarianteController::class, 'show']);
+
+
+// Route::prefix('admin')->group(function () {
+
+// })->middleware('auth:sanctum');
+
+// Route::prefix('user')->group(function () {
+
+// })->middleware('auth:sanctum');
+
+
+/*
+     Routes necessitant l'authentification
+ */
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Variantes
+    Route::post('/variantes', [VarianteController::class, 'store']);
+    Route::put('/variantes/{variante}', [VarianteController::class, 'update']);
+    Route::delete('/variantes/{variante}', [VarianteController::class, 'destroy']);
+
+
 });
