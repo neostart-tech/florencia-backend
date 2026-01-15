37a31b7a66138f10cecfe4ca62c9bf0e5a042da8
edenjordan <jordandokou1@gmail.com>
2026-01-15 08:56:57 +0000

    Reajustement du modele image

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -9,11 +9,26 @@ class Article extends Model
 {
     use HasUuid;
 
-    protected $fillable = ['nom','prix','description','sous_categorie_id'];
+    protected $fillable = ['nom', 'prix', 'description', 'sous_categorie_id'];
 
-    public function sousCategorie(){ return $this->belongsTo(Sous_categorie::class); }
-    public function stock(){ return $this->hasOne(Stock::class); }
-    public function images(){ return $this->hasMany(Image::class); }
-    public function variantes(){ return $this->belongsToMany(Variante::class, 'articles_variantes'); }
-    public function mouvements(){ return $this->hasMany(Stock_mouvement::class); }
+    public function sousCategorie()
+    {
+        return $this->belongsTo(Sous_categorie::class);
+    }
+    public function stock()
+    {
+        return $this->hasOne(Stock::class);
+    }
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'owner');
+    }
+    public function variantes()
+    {
+        return $this->belongsToMany(Variante::class, 'articles_variantes');
+    }
+    public function mouvements()
+    {
+        return $this->hasMany(Stock_mouvement::class);
+    }
 }
