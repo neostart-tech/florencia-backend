ce5a2c7d586aba4bf015b0874e31ba1e58e6238e
edenjordan <jordandokou1@gmail.com>
2026-01-19 11:37:47 +0000

    Logique et fonctions pour la gestion de stock

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\profil\ProfilController;
 use App\Http\Controllers\services\ServiceController;
 use App\Http\Controllers\sousCategories\SousCategorieController;
 use App\Http\Controllers\adresses\AdresseController;
+use App\Http\Controllers\stocks\StockController;
 use App\Http\Controllers\users\AuthController as AuthUser;
 use App\Http\Controllers\variantes\VarianteController;
 use Illuminate\Http\Request;
@@ -54,6 +55,9 @@ Route::get('/services/{service}', [ServiceController::class, 'show']);
 // Articles
 Route::get('/articles', [ArticleController::class, 'index']);
 Route::get('/articles/{article}', [ArticleController::class, 'show']);
+// Stocks
+Route::get('/stocks', [StockController::class, 'index']);
+Route::get('/stocks/{article}', [StockController::class, 'show']);
 
 
 // Route::prefix('admin')->group(function () {
@@ -128,6 +132,13 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('/{article}', [ArticleController::class, 'update']);
         Route::delete('/{article}', [ArticleController::class, 'destroy']);
     });
+
+    // Gestion du stock
+    Route::prefix('/stocks')->group(function () {
+        Route::get('/{article}/mouvements', [StockController::class, 'mouvements']);
+        Route::post('/mouvement', [StockController::class, 'store']);
+    });
+
 
     // Gestion des administrateurs (superadmin seulement)
     Route::prefix('/admins')->group(function () {
